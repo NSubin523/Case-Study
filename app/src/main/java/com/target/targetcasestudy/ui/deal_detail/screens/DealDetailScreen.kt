0a0c52929e12b959bbd43862eb505a5a65292672
Subin <neosubin729@gmail.com>
2025-06-10 08:53:59 -0400

    chore: save changes

--- app/src/main/java/com/target/targetcasestudy/ui/deal_detail/screens/DealDetailScreen.kt
+++ app/src/main/java/com/target/targetcasestudy/ui/deal_detail/screens/DealDetailScreen.kt
@@ -14,11 +14,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.text.SpanStyle
-import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.withStyle
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
