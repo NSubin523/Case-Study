c94062aff82e1c590598c9042d401b41e0290d63
Subin <neosubin729@gmail.com>
2025-06-11 16:46:40 -0400

    chore: save changes

--- app/src/androidTest/java/com/target/targetcasestudy/ui/deal_list/FakeRetrieveDeals.kt
+++
@@ -1,7 +0,0 @@
-package com.target.targetcasestudy.ui.deal_list
-
-import com.target.targetcasestudy.data.model.Deal
-
-class FakeRetrieveDeals(private val deals: List<Deal>) {
-    suspend operator fun invoke(): List<Deal> = deals
-}
